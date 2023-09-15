283f8108cd898cee69d545e64af225afb34020e3
naz kutub <nazkutub@gmail.com>
2023-09-15 14:29:20 +0800

    editing addTextField closure

---
+++ Todoey/Models/K.swift
@@ -0,0 +1,14 @@
+//
+//  K.swift
+//  Todoey
+//
+//  Created by Naz on 9/14/23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    
+    static let cellIdentifier = "ToDoItemCell"
+}
---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    
+    var itemArray = [
+        "Stranger Things",
+        "Open Box",
+        "Stupider Worsters"
+    ]
+    
+
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //MARK: Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+
+        content.text = itemArray[indexPath.row]
+        
+        cell.contentConfiguration = content
+        
+        
+        return cell
+        
+    }
+    
+    //MARK: Tableview Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+   //     print(itemArray[indexPath.row])
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if let cell = tableView.cellForRow(at: indexPath) {
+            if cell.accessoryType == .checkmark {
+                cell.accessoryType = .none
+            } else {
+                cell.accessoryType = .checkmark
+            }
+        }
+
+    }
+    
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        let alert = UIAlertController(title: "Add new item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default)
+        
+        alert.addAction(action)
+        alert.addTextField { (alertTextField) in
+            
+            alertTextField.placeholder = "Type it out"
+            
+        }
+        
+        present(alert, animated: true)
+        
+//        itemArray.append("")
+//        tableView.reloadData()
+        
+    }
+    
+
+
+
+}
+
